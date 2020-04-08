79db08142dea37aa08b4699468d91c68bdd49699
Jnmendza <jonatmendoza@gmail.com>
2020-04-08 15:18:31 -0700

    last_block endpoint, valid_proof -> 6 0s, modifying POST

---
+++ hello.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "<h1>Hello, world!</h1>"
+
+
+@app.route("/newpage")
+def newPage():
+    return "<h1>New Page</h1>"
+
+
+if __name__ == "__main__":
+    app.run()
